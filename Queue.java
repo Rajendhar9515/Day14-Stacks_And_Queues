c7b8d1f04f0ffbb56eb83bf52a4b421abd4f708e
Rajendhar9515 <vemula.rajendhar1@gmail.com>
2022-08-03 12:22:01 +0530

    Ability to dequeue(FIFO) is solved.

--- Queue.java
+++ Queue.java
@@ -3,29 +3,36 @@ package com.stacksandqueues;
 import java.util.LinkedList;
 
 public class Queue {
-	// created object of LinkedList class
+	
+	// created object of LinkedList class 
 	LinkedList<Integer> linkedList = new LinkedList<Integer>();
 
-	// Adding enqueue method
+	// Adding enqueue method 
 	public void enqueue(Integer data) {
 		linkedList.addLast(data);
 	}
 
-	// Printing queue method.
+	// adding dequeue method. 
+	public void dequeue() {
+		linkedList.removeFirst();
+	}
+
+	// Printing queue method. 
 	public void printQueue() {
 		System.out.println("\n" + linkedList.toString());
 	}
 
-	// Main method.
+	// Main method. 
 	public static void main(String[] args) {
-
-		// Displayed welcome message
+		// Displayed welcome message 
 		System.out.println("\t WELCOME TO QUEUE \n");
 
 		Queue queue = new Queue();
-		queue.enqueue(56); // adding 56 first
-		queue.enqueue(30); // adding 30 second
-		queue.enqueue(70); // adding 70 last
+		queue.enqueue(56);           // adding 56 first
+		queue.enqueue(30);           // adding 30 second
+		queue.enqueue(70);           // adding 70 last
+		queue.printQueue();
+		queue.dequeue();             // Since queue is follows FIFO . so first element will be deleted first.
 		queue.printQueue();
 	}
 }
